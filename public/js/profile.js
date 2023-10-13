23e95442241506054f3b9d01f0425f9b272d0a97
unknown <gptv88@gmail.com>
2023-10-12 19:39:36 -0700

    new update

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,13 +2,13 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
-
-  if (name && needed_funding && description) {
+  const calories  = document.querySelector('#project-funding').value.trim();
+  const ingredients = document.querySelector('#project-desc').value.trim();
+  const instructions = document.querySelector('#project-inst')
+  if (name && calories && ingredients && instructions ) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ name, calories, ingredients, instructions }),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -44,4 +44,5 @@ document
 
 document
   .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .addEventListener('click',delButtonHandler);
+  
